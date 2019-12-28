86f3c4e01898385dbe77414f4536902725472ddd
Daniel Benitez <dbenitezcuenca@gmail.com>
2019-12-28 00:16:40 -0500

    Implementacion de la capa de servicios (Clean Arquitecture) crud completo

--- routes/movies.js
+++ routes/movies.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const { moviesMock } = require('../utils/mocks/movies.js'); //los mocks son archivos falsos de datos falsos (como ejemplo), also help test
+const MoviesService = require('../services/movies');
 
 function moviesApi(app) {
     const router = express.Router();
     app.use("/api/movies", router); // "/api/movies" => es el home de la aplicacion
 
+    // con esto accedemos a la capa de servicio
+    const moviesService = new MoviesService();
+
     // con "/" hacemos referencia al home definido
     router.get("/", async function(req, res, next){
+        const { tags } = req.query; // tipo de parametro1: (query) se pone el ? "nombredelquery" y se concatena "tags"
+
         try {
-            const movies = await Promise.resolve(moviesMock);
+            const movies = await moviesService.getMovies({ tags })
 
             res.status(200).json({
                 data: movies,
@@ -22,9 +27,10 @@ function moviesApi(app) {
     });
     
     // METODO GET CON ID
-    router.get("/:movieId", async function(req, res, next){
+    router.get("/:movieId", async function(req, res, next){ // tipo de parametro2: le enviamos el parametro a traves de la URL
+        const { movieId } = req.params;
         try {
-            const movie = await Promise.resolve(moviesMock[req.params.movieId]);
+            const movie = await Promise.resolve({ movieId });
 
             res.status(200).json({
                 data: movie,
@@ -39,9 +45,10 @@ function moviesApi(app) {
 
     // METODO POST PARA CREAR UN ELEMENTO
     router.post("/", async function(req, res, next){
+        const { body: movie } = req;
         try {
-            const createdMovieId = await Promise.resolve(moviesMock[0].id);
-
+            const createdMovieId = await moviesService.createMovie({ movie });
+            
             res.status(201).json({
                 data: createdMovieId,
                 message: 'movie created'
@@ -55,8 +62,10 @@ function moviesApi(app) {
 
     // METODO PUT PARA ACTUALIZAR UN ELEMENTO
     router.put("/:movieId", async function(req, res, next){
+        const { movieId } = req.params;
+        const { body: movie } = req;
         try {
-            const updateMovieId = await Promise.resolve(moviesMock[0].id);
+            const updateMovieId = await moviesService.updateMovie({ movieId, movie });
 
             res.status(200).json({
                 data: updateMovieId,
@@ -71,8 +80,9 @@ function moviesApi(app) {
 
     // METODO DELETE PARA ELIMINAR UN ELEMENTO
     router.delete("/:movieId", async function(req, res, next){
+        const { movieId } = req.params;
         try {
-            const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+            const deletedMovieId = await moviesService.deleteMovie({ movieId });
 
             res.status(200).json({
                 data: deletedMovieId,
---
+++ services/movies.js
@@ -0,0 +1,31 @@
+//los services son el corazon de nuestra app
+const { moviesMock } = require("../utils/mocks/movies");
+
+class MoviesService {
+    async getMovies() {
+        const movies = await Promise.resolve(moviesMock);
+        return movies || [];
+    }
+
+    async getMovie() {
+        const movie = await Promise.resolve(moviesMock[0]);
+        return movie || {};
+    }
+
+    async createMovie() {
+        const createMovieId = await Promise.resolve(moviesMock[0].id);
+        return createMovieId;
+    }
+
+    async updateMovie() {
+        const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+        return updatedMovieId;
+    }
+
+    async deleteMovie() {
+        const deletedMovie = await Promise.resolve(moviesMock[0].id);
+        return deletedMovie;
+    }
+}
+
+module.exports = MoviesService;
