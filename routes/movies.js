44059d3f4cc2144fec6509dc8455653b7c99271d
Daniel Benitez <dbenitezcuenca@gmail.com>
2019-12-27 19:48:38 -0500

    Crud: get id, post, put, delete; configuracion gitignore(https://www.gitignore.io/)

--- routes/movies.js
+++ routes/movies.js
@@ -5,6 +5,7 @@ function moviesApi(app) {
     const router = express.Router();
     app.use("/api/movies", router); // "/api/movies" => es el home de la aplicacion
 
+    // con "/" hacemos referencia al home definido
     router.get("/", async function(req, res, next){
         try {
             const movies = await Promise.resolve(moviesMock);
@@ -18,7 +19,71 @@ function moviesApi(app) {
         } catch (error) {
             next(error);
         }
-    }); // con "/" hacemos referencia al home definido
+    });
+    
+    // METODO GET CON ID
+    router.get("/:movieId", async function(req, res, next){
+        try {
+            const movie = await Promise.resolve(moviesMock[req.params.movieId]);
+
+            res.status(200).json({
+                data: movie,
+                message: 'movie retrieved'
+            });
+
+
+        } catch (error) {
+            next(error);
+        }
+    }); 
+
+    // METODO POST PARA CREAR UN ELEMENTO
+    router.post("/", async function(req, res, next){
+        try {
+            const createdMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(201).json({
+                data: createdMovieId,
+                message: 'movie created'
+            });
+
+
+        } catch (error) {
+            next(error);
+        }
+    }); 
+
+    // METODO PUT PARA ACTUALIZAR UN ELEMENTO
+    router.put("/:movieId", async function(req, res, next){
+        try {
+            const updateMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data: updateMovieId,
+                message: 'movie updated'
+            });
+
+
+        } catch (error) {
+            next(error);
+        }
+    }); 
+
+    // METODO DELETE PARA ELIMINAR UN ELEMENTO
+    router.delete("/:movieId", async function(req, res, next){
+        try {
+            const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data: deletedMovieId,
+                message: 'movie deleted'
+            });
+
+
+        } catch (error) {
+            next(error);
+        }
+    }); 
 }
 
 module.exports = moviesApi;
