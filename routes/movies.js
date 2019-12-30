00497f1beeeaca673025014866b426250455bbe3
Daniel Benitez <dbenitezcuenca@gmail.com>
2019-12-29 23:33:16 -0500

    Implementacion de joi: creacion de schemas, y uso del validationHandler en routes

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const MoviesService = require('../services/movies');
 
+// reglas de validacion
+const {
+    movieIdSchema,
+    createMovieSchema,
+    updateMovieSchema
+} = require('../utils/schemas/movies')
+
+const validationHandler = require('../utils/middleware/validationHandler');
+
 function moviesApi(app) {
     const router = express.Router();
     app.use("/api/movies", router); // "/api/movies" => es el home de la aplicacion
@@ -9,7 +18,7 @@ function moviesApi(app) {
     const moviesService = new MoviesService();
 
     // con "/" hacemos referencia al home definido
-    router.get("/", async function(req, res, next){
+    router.get("/", async function (req, res, next) {
         const { tags } = req.query; // tipo de parametro1: (query) se pone el ? "nombredelquery" y se concatena "tags"
 
         try {
@@ -28,78 +37,91 @@ function moviesApi(app) {
             next(error);
         }
     });
-    
+
     // METODO GET CON ID
-    router.get("/:movieId", async function(req, res, next){ // tipo de parametro2: le enviamos el parametro a traves de la URL
-        const { movieId } = req.params;
-        try {
-            const movie = await moviesService.getMovie({ movieId });
+    router.get(
+        "/:movieId", // tipo de parametro2: le enviamos el parametro a traves de la URL
+        validationHandler({ movieId: movieIdSchema }, 'params'),
+        async function (req, res, next) {
+            const { movieId } = req.params;
+            try {
+                const movie = await moviesService.getMovie({ movieId });
 
-            res.status(200).json({
-                data: movie,
-                message: 'movie retrieved'
-            });
+                res.status(200).json({
+                    data: movie,
+                    message: 'movie retrieved'
+                });
 
 
-        } catch (error) {
-            next(error);
-        }
-    }); 
+            } catch (error) {
+                next(error);
+            }
+        });
 
     // METODO POST PARA CREAR UN ELEMENTO
-    router.post("/", async function(req, res, next){
-        const { body: movie } = req;
-        try {
-            const createdMovieId = await moviesService.createMovie({ movie });
-            
-            res.status(201).json({
-                data: createdMovieId,
-                message: 'movie created'
-            });
+    router.post(
+        "/",
+        validationHandler(createMovieSchema),
+        async function (req, res, next) {
+            const { body: movie } = req;
+            try {
+                const createdMovieId = await moviesService.createMovie({ movie });
+
+                res.status(201).json({
+                    data: createdMovieId,
+                    message: 'movie created'
+                });
 
 
-        } catch (error) {
-            next(error);
-        }
-    }); 
+            } catch (error) {
+                next(error);
+            }
+        });
 
     // METODO PUT PARA ACTUALIZAR UN ELEMENTO
-    router.put("/:movieId", async function(req, res, next){
-        const { movieId } = req.params;
-        const { body: movie } = req;
-        try {
-            const updateMovieId = await moviesService.updateMovie({ movieId, movie });
+    router.put(
+        "/:movieId",
+        validationHandler({ movieId: movieIdSchema }, 'params'),
+        validationHandler(updateMovieSchema),
+        async function (req, res, next) {
+            const { movieId } = req.params;
+            const { body: movie } = req;
+            try {
+                const updateMovieId = await moviesService.updateMovie({ movieId, movie });
 
-            res.status(200).json({
-                data: updateMovieId,
-                message: 'movie updated'
-            });
+                res.status(200).json({
+                    data: updateMovieId,
+                    message: 'movie updated'
+                });
 
 
-        } catch (error) {
-            next(error);
-        }
-    }); 
+            } catch (error) {
+                next(error);
+            }
+        });
 
     // METODO DELETE PARA ELIMINAR UN ELEMENTO
-    router.delete("/:movieId", async function(req, res, next){
-        const { movieId } = req.params;
-        try {
-            const deletedMovieId = await moviesService.deleteMovie({ movieId });
+    router.delete(
+        "/:movieId",
+        validationHandler({ movieId: movieIdSchema }, 'params'),
+        async function (req, res, next) {
+            const { movieId } = req.params;
+            try {
+                const deletedMovieId = await moviesService.deleteMovie({ movieId });
 
-            res.status(200).json({
-                data: deletedMovieId,
-                message: 'movie deleted'
-            });
+                res.status(200).json({
+                    data: deletedMovieId,
+                    message: 'movie deleted'
+                });
 
 
-        } catch (error) {
-            next(error);
-        }
-    });
+            } catch (error) {
+                next(error);
+            }
+        });
 
     // METODO PATCH PARA ACTUALIZAR LA PROPIEDAD DE UN ELEMENTO
-    router.patch("/:movieId", async function(req, res, next){
+    router.patch("/:movieId", async function (req, res, next) {
         const { movieId } = req.params;
         const { body: movie } = req;
         try {
