de954e56c7e1f2a4d965d0c74eb3637beacfa1cc
Daniel Benitez <dbenitezcuenca@gmail.com>
2019-12-29 22:30:45 -0500

    Implementando Boom en los middleware; captura del error 404-notFound

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ const { config } = require('./config/index');
 
 const moviesApi = require('./routes/movies.js'); // es una funcion
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers.js');
+const { logErrors, errorHandler, wrapErrors } = require('./utils/middleware/errorHandlers.js');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // middleware body parser
 app.use(express.json()); // esto va a permitir que los datos de tipo json sean interpretados (POST)
 
+// routes
 moviesApi(app);
+
+// capturar el error 404 (cuando se cambia la ruta "URL" q no esta definido en el api)
+app.use(notFoundHandler);
 
 // middleware de manejo de errores
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, ()=>{
